40fd85406d7fb986dcef43a2594fae8091c97a51
Sullivan <38718448+Epic-R-R@users.noreply.github.com>
2023-01-11 16:30:54 +0300

    Movie data interface

---
+++ src/types/movieData.ts
@@ -0,0 +1,13 @@
+interface MovieData {
+  thumbnail: string | undefined;
+  title: string;
+  sendData: string;
+  reviews: string;
+  translators: string;
+  formatSubtitle: string;
+  versions: string;
+  imdbLink: string | undefined;
+  subtitleLink: string | undefined;
+}
+
+export { MovieData };
